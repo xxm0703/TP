21ec3152947f3cdd568a3cb1f342d2443641cafe
Martin Jordanov <xxm0703@abv.bg>
2018-10-28 12:13:08 +0200

    [Update] Multiple levels

--- 03-10-2018-objects/jquery.js
+++ 03-10-2018-objects/jquery.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
 	$('#btnInnerNode').click(function() {
 		innerAttributeControlls(_state, $('#items'));
-	});	
+	});
 
 });
 function showState(state) {
@@ -27,19 +27,34 @@ function innerAttributeControlls(state, parent) {
 	var jsonNodeLocalKey = $("<input />", {id: 'localKey'});
 	var colon = $("<span />", {text : ":"});
 	var jsonNodeValue = $("<input />", {id: 'value'});
+	var inner = $("<button />", {text:'Add Inner'});
+	var newNode = $("<button />", {text:'Add Node'});
 	var update = $("<button />", {text:'Update'});
 	var remove = $("<button />", {text:'Remove'});
 	var br = $("<br />");
 
 	$(update).click(function() {
+		console.log(state );
 		var paren = $(this).parent();
 		var gKey = $(paren).children('#globalKey').val();
 		var lKey = $(paren).children('#localKey').val();
 		var value = $(paren).children('#value').val();
 		innerObj = {};
-		innerObj[lKey] = value; 
+		innerObj[lKey] = value;
 		state[gKey] = innerObj;
+
 		state = state[gKey];
+	});
+
+	$(newNode).click(function() {
+		var paren = $(this).parent();
+		var gKey = $(paren).children('#globalKey').val();
+		createNewAttributeControlls(state, span);
+	});
+
+	$(inner).click(function() {
+		console.log(state);
+		innerAttributeControlls(state, span);
 	});
 
 	$(remove).click(function() {
@@ -52,7 +67,9 @@ function innerAttributeControlls(state, parent) {
 	span.append(jsonNodeLocalKey);
 	span.append(colon);
 	span.append(jsonNodeValue);
+	span.append(newNode);
 	span.append(update);
+	span.append(inner);
 	span.append(remove);
 	span.append(br);
 
