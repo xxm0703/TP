09696056de40aac68a1d96e8a064e97616ba071a
Martin_Jordanov <martistj@gmail.com>
2019-01-10 23:32:20 +0200

    [Update] Store and Upgrade bar

--- Asteroids/Assets/Scripts/GameOverMenuHandler.cs
+++ Asteroids/Assets/Scripts/GameOverMenuHandler.cs
@@ -6,13 +6,27 @@ using UnityEngine.SceneManagement;
 public class GameOverMenuHandler : MonoBehaviour {
 
 	public string GameScene = "Main";
+	public string ShopScene = "Shop";
+	public string GameOverScene = "GameOver";
+
 	public void RestartGame()
 	{
+		Debug.Log("smth1");
 		SceneManager.LoadScene(GameScene);
 	}
 
+	public void EnterShop()
+	{
+		SceneManager.LoadScene(ShopScene);
+	}
+		
+	public void GameOver()
+	{
+		SceneManager.LoadScene(GameOverScene);
+	}
+		
 	public void QuitGame()
 	{
-		Application.Quit ();
+		Application.Quit();
 	}
 }
