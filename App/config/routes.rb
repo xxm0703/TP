0fcba69d285577fe33988b9b70a31daa9bf9e5de
Martin_Jordanov <martistj@gmail.com>
2019-01-15 22:21:50 +0200

    [Add] Creating new equations happens through an user profile

--- App/config/routes.rb
+++ App/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'pages#index'
+  root to: 'pages#index'
 
-  resources :equations
+  resources :equations, except: [:new]
   resources :individuals
 
   get 'equations/:id/render_writers', to: 'equations#render_writers'
   get 'equations/:id/add_writer/:writer_id', to: 'equations#add_writer'
+  get 'individuals/:id/new_equation', to: 'equations#new'
 end
