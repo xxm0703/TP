51d9bc3f6d12b4c995bcb9d91a66f00d07c43b1e
Martin_Jordanov <martistj@gmail.com>
2019-01-16 20:54:22 +0200

    [Fix] Table with equations only if exists

--- App/app/controllers/individuals_controller.rb
+++ App/app/controllers/individuals_controller.rb
@@ -1,13 +1,13 @@
 class IndividualsController < ApplicationController
   def new
-    @individual = Individual.new
+    @person = Individual.new
   end
 
   def create
-    @individual = Individual.new(individual_params)
+    @person = Individual.new(individual_params)
 
-    if @individual.save
-      redirect_to @individual
+    if @person.save
+      redirect_to @person
     else
       render 'new'
     end
--- App/app/models/individual.rb
+++ App/app/models/individual.rb
@@ -1,4 +1,4 @@
 class Individual < ApplicationRecord
-	has_many :equations, foreign_key: "writer_id"
+	has_many :equations, foreign_key: "writer_id", dependent: :destroy
 	validates :name, length: {minimum: 2}
 end
